7722420c94807760d7369e37b2781c971240fd7b
ncoudsi <nicolas.coudsi@gmail.com>
2020-07-04 11:28:50 +0200

    refined parsing [stable]

--- srcs/structures/map_params/utils/parsing/params/colors_parsing.c
+++ srcs/structures/map_params/utils/parsing/params/colors_parsing.c
@@ -1,18 +1,39 @@
 #include "cube.h"
 
-static t_bool   is_color_value(char *color_value)
+static t_bool   is_color_value(char *color_line, int index)
+{
+    int value;
+    
+    value = ft_atoi(color_line + index);
+    if (value > 255)
+        return (false);
+    return (true);
+}
+
+static t_bool   is_valid_color(char *color_line)
 {
     int index;
-    int len;
+    int sep_number;
 
     index = 0;
-    len = ft_strlen(color_value);
-    while (index < len)
+    sep_number = 0;
+    while (color_line[index] != '\0')
     {
-        if (ft_is_digit(color_value[index]) == false && color_value[index] != ',')
+        if (ft_is_digit(color_line[index]) == false && color_line[index] != ',')
+            return (false);
+        else if (color_line[index] == ',')
+        {
+            sep_number++;
+            if (ft_is_digit(color_line[index + 1]) == false)
+                return (false);
+        }
+        else if (ft_is_digit(color_line[index]) == true &&
+                is_color_value(color_line, index) == false)
             return (false);
         index++;
     }
+    if (sep_number != 2)
+        return (false);
     return (true);
 }
 
@@ -24,6 +45,6 @@ void    colors_parsing(char **colors_line)
         error_exit(COL_ERROR);
     else if (ft_strlen(colors_line[1]) > 11 || ft_strlen(colors_line[1]) < 5)
         error_exit(COL_ERROR);
-    else if (is_color_value(colors_line[1]) == false)
+    else if (is_valid_color(colors_line[1]) == false)
         error_exit(COL_ERROR);
 }
