878c32d4e955b95c20cce42ca548cb60d89d550d
ncoudsi <nicolas.coudsi@gmail.com>
2020-09-18 12:35:40 +0200

    changed wall textures [stable]

--- srcs/graphics/run_game.c
+++ srcs/graphics/run_game.c
@@ -15,7 +15,9 @@
 void	run_game()
 {
     set_img_ptr(mlx_new_image(mlx_ptr(), resolution_x(), resolution_y()));
-	set_pixels(mlx_get_data_addr(img_ptr(), &g_engine->mlx_params->bits_per_pixel, &g_engine->mlx_params->size_line, &g_engine->mlx_params->endian));
+	set_pixels(mlx_get_data_addr(img_ptr(),
+	&g_engine->mlx_params->bits_per_pixel, &g_engine->mlx_params->size_line,
+	&g_engine->mlx_params->endian));
 	mlx_loop_hook (mlx_ptr(), &update_game, NULL);
 	mlx_hook(win_ptr(), KEY_PRESS, (1L << 0), &key_press, NULL);
 	mlx_hook(win_ptr(), KEY_RELEASE, (1L << 1), &key_release, NULL);
